37d2769180150bc310afdac9d8e18820f905d484
TolgaErtugrul <107127720+TolgaErtugrul@users.noreply.github.com>
2023-12-10 17:30:29 +0300

    https://github.com/TolgaErtugrul/Java.git

--- Odev2.java
+++ Odev2.java
@@ -16,6 +16,34 @@ public class Odev2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    /*
+        Kahraman güç puanı 2, 3 veya 4 olabilir.
+        Zorluk seviyesi 1 veya 2 olacaktır.
+        1. zorluk seviyesi seçilirse zorluk katsayısı 0.6 ve 2 zorluk seviyesi seçilirse zorluk katsayısı 1.0 olacaktır.
+        Görev, ilgili görevdeki kahramanların güç toplamının, görev zorluğu ile oyun zorluk katsayısının çarpımından büyük olması durumunda başarılı kabul edilir. Eşit olduğu durumda başarısız olacaktır.
+    Başarılı durum: Görevdeki kahramanların güç toplamı > görev zorluğu * oyun zorluk katsayısı
+    1. görevde sadece 1. kahraman olacaktır. Diğer görevlerde bütün kahramanlar olacaktır.
+    Görev zorlukları sırasıyla 3, 6 ve 10 olacaktır.
+    Çıktı aşağıdaki şekilde olacaktır:
+    Görev 1: BAŞARILI/BAŞARISIZ
+    Görev 2: BAŞARILI/BAŞARISIZ
+    Görev 3: BAŞARILI/BAŞARISIZ
+    Hatalı girdi durumları ele alınmayacaktır. 
+    Kahraman Bilgileri:
+
+    Kahraman 1 Güç Puanı: [2 ile 4 arasında bir sayı]
+    Kahraman 2 Güç Puanı: [2 ile 4 arasında bir sayı]
+    Zorluk Katsayıları:
+
+    [1] Birinci Zorluk Katsayısı: 0.6
+    [2] İkinci Zorluk Katsayısı: 1.0
+    Görev Zorluklari:
+
+    Görev 1: Zorluk 3
+    Görev 2: Zorluk 6
+    Görev 3: Zorluk 10
+    */
+
         int kahraman1 = Integer.parseInt(JOptionPane.showInputDialog("Kahraman 1 Güç Puanı Giriniz:")) ;
         int kahraman2 = Integer.parseInt(JOptionPane.showInputDialog("Kahraman 2 Güç Puanı Giriniz:"));
         int zorlukSecim = 0;
